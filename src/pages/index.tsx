555a9ecc089f25520360500eb2d1e53f0bf36cf4
Robert Smieja <robertsmieja@users.noreply.github.com>
2019-05-18 23:51:20 -0400

    Update index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,16 @@
 import React from "react"
 
-import Image from "../components/image"
+import Container from "react-bootstrap/Container"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`robertsmieja`, `robert`, `smieja`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    {/* <Link to="/page-2/">Go to page 2</Link> */}
+    <Container>
+      <h1>Welcome my personal site.</h1>
+      <p>Currently still under construction.</p>
+    </Container>
   </Layout>
 )
 
