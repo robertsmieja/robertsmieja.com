5f257595c0e01662462ba720c09c088984fc6d95
Robert Smieja <robertsmieja@users.noreply.github.com>
2019-05-29 23:44:32 -0400

    Update manifest in gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `Robert Smieja`,
-    description: ``,
+    description: `Personal homepage for Robert Smieja`,
     author: `Robert Smieja`,
   },
   plugins: [
@@ -18,13 +18,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Robert Smieja`,
+        short_name: `Robert Smieja`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        // background_color: `#663399`,
+        // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        // icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
