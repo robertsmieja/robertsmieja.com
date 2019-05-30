338bd29b862503eaac8e76df41cbf759a06f3655
Robert Smieja <robertsmieja@users.noreply.github.com>
2019-05-29 23:05:17 -0400

    Update index page with some content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 
+import { Link } from "gatsby"
 import Container from "react-bootstrap/Container"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -9,7 +10,11 @@ const IndexPage = () => (
     <SEO title="Home" keywords={[`robertsmieja`, `robert`, `smieja`]} />
     <Container>
       <h1>Welcome my personal site.</h1>
-      <p>Currently still under construction.</p>
+      <p>I am a Full-stack Software/DevOps Engineer.</p>
+      <p>
+        This site is under construction, I recommend checking out my{" "}
+        <Link to="/resume">résumé</Link> for now.
+      </p>
     </Container>
   </Layout>
 )
