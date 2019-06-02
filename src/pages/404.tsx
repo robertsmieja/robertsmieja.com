24acbe951651915f8f1affe36e3679b90a7b1bfe
Robert Smieja <robertsmieja@users.noreply.github.com>
2019-06-01 23:38:04 -0400

    Update 404 page with Bootstrap

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,13 +1,16 @@
 import React from "react"
 
+import Container from "react-bootstrap/Container"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>404, Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist...</p>
+    <Container>
+      <h1>404, Not Found</h1>
+      <p>You just hit a route that doesn&#39;t exist...</p>
+    </Container>
   </Layout>
 )
 
