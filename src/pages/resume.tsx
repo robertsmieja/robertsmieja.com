58bc7e80390305f6d05855ea940ca2fad76c6dd3
Robert Smieja <robertsmieja@users.noreply.github.com>
2019-06-01 22:07:23 -0400

    Add brand icons + create lists

--- src/pages/resume.tsx
+++ src/pages/resume.tsx
@@ -1,8 +1,15 @@
 import React from "react"
 
 import {
+  faApple,
+  faJava,
+  faJsSquare,
+  faRedhat,
+  faWindows,
+} from "@fortawesome/free-brands-svg-icons"
+import {
   faBriefcase,
-  faTasks,
+  faCode,
   faUniversity,
 } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
@@ -14,7 +21,17 @@ const ResumePage = () => (
   <Layout>
     <SEO
       title="Résumé"
-      keywords={[`robertsmieja`, `robert`, `smieja`, `resume`, `résumé`]}
+      keywords={[
+        `robertsmieja`,
+        `robert`,
+        `smieja`,
+        `resume`,
+        `résumé`,
+        `wpi`,
+        `worcester`,
+        `polytechnic`,
+        `institute`,
+      ]}
     />
     <Container>
       <h2>Objective</h2>
@@ -83,20 +100,67 @@ const ResumePage = () => (
       </dl>
 
       <h2>
-        <FontAwesomeIcon icon={faTasks} /> Computer Skills
+        <FontAwesomeIcon icon={faCode} /> Technologies
       </h2>
       <dl>
         <dt>Operating Systems</dt>
-        <dd>Windows, Mac OS X, Linux (RHEL)</dd>
+        <dd>
+          <ul>
+            <li>
+              <FontAwesomeIcon icon={faWindows} /> Windows
+            </li>
+            <li>
+              <FontAwesomeIcon icon={faApple} /> Mac OS X
+            </li>
+            <li>
+              <FontAwesomeIcon icon={faRedhat} /> Linux (RHEL)
+            </li>
+          </ul>
+        </dd>
 
         <dt>Programming Languages</dt>
-        <dd>C#, C/C++, Java, Groovy, SQL</dd>
+        <dd>
+          In order of proficiency:
+          <ol>
+            <li>
+              {/* <FontAwesomeIcon icon={faJava} />  */}
+              Java
+            </li>
+            <li>Groovy</li>
+            <li>SQL</li>
+            <li>Kotlin</li>
+            <li>TypeScript</li>
+            <li>
+              {/* <FontAwesomeIcon icon={faJsSquare} /> */}
+              JavaScript
+            </li>
+            <li>Ruby</li>
+            <li>C#</li>
+            <li>Perl</li>
+            <li>C/C++</li>
+            <li>Scala</li>
+          </ol>
+        </dd>
 
-        <dt>Technologies/Frameworks</dt>
-        <dd>DB2 (LUW),Grails, Gradle, Spring MVC</dd>
+        <dt>Frameworks and Libraries</dt>
+        <dd>
+          <ul>
+            <li>Grails</li>
+            <li>Gradle</li>
+            <li>Spring MVC</li>
+            <li>Spring Data</li>
+          </ul>
+        </dd>
+
+        <dt>Databases</dt>
+        <dd>
+          <ul>
+            <li>DB2 (LUW)</li>
+            <li>Amazon DynamoDB</li>
+          </ul>
+        </dd>
       </dl>
     </Container>
-    {/* <Link to="/page-2/">Go to page 2</Link> */}
   </Layout>
 )
 
