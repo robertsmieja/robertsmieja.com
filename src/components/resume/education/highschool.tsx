720b11ab9014872135c071dff9c801791c4bdb0a
Robert Smieja <robertsmieja@users.noreply.github.com>
2019-06-03 20:03:12 -0400

    Split education into college + highschool

---
+++ src/components/resume/education/highschool.tsx
@@ -0,0 +1,22 @@
+import React from "react"
+
+const HighSchool = () => (
+  <div id="highschool">
+    <dt>
+      University of Hartford High School of Science and Engineering, Hartford,
+      CT High School Diploma, June 2011
+    </dt>
+    <dd>
+      College level courses taken at University of Hartford:
+      <ul>
+        <li>Computer Programming I</li>
+        <li>Engineering Computer Applications</li>
+        <li>Calculus I</li>
+        <li>Humanoid Robotics</li>
+        <li>Graphic Communication</li>
+      </ul>
+    </dd>
+  </div>
+)
+
+export default HighSchool
