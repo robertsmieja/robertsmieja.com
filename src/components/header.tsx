d26fd3544ace33135f35fa009ef9253900b6d5b8
Robert Smieja <robertsmieja@users.noreply.github.com>
2019-06-03 23:58:23 -0400

    Update header colors

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -14,10 +14,12 @@ interface HeaderProperties {
 
 const Header = ({ siteTitle }: HeaderProperties) => (
   <header>
-    <Navbar bg="light" expand="lg">
-      <Navbar.Brand>
-        <Link to="/">{siteTitle}</Link>
-      </Navbar.Brand>
+    <Navbar bg="dark" variant="dark" expand="lg">
+      {/* <Navbar.Brand> */}
+      <Link className="navbar-brand" to="/">
+        {siteTitle}
+      </Link>
+      {/* </Navbar.Brand> */}
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
