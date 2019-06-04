54021650ba2af6b8346c4d5b5e1fcd342d93720a
Robert Smieja <robertsmieja@users.noreply.github.com>
2019-06-03 22:34:56 -0400

    Breakdown experience in resume to components

--- src/components/resume/experience.tsx
+++ src/components/resume/experience.tsx
@@ -1,50 +1,49 @@
 import { faBriefcase } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import React from "react"
+import ExperienceEntry from "./experience/experienceEntry"
 
 const Experience = () => (
   <div id="experience">
     <h2 className="font-weight-light">
       <FontAwesomeIcon icon={faBriefcase} /> Experience
     </h2>
-    <dl>
-      <dt>
-        <p>Vantiv, now WorldPay, Lowell, MA, August 2017 – Present</p>
-        <p>Senior Software Engineer</p>
-      </dt>
-      <dd>
-        Worked on the WorldPay integration with AmazonPay as the first acquirer
+    <ExperienceEntry
+      company="Vantiv, now WorldPay"
+      location="Lowell, MA"
+      dateRange="August 2017 – Present"
+      jobTitle="Senior Software Engineer"
+      description="Worked on the WorldPay integration with AmazonPay as the first acquirer
         to partner with AmazonPay. Currently working as a Automation/DevOps
         working on development and production infrastructure automation in
-        Puppet.
-      </dd>
-      <dt>
-        <p>Vantv - eCommerce, Lowell, MA, May 2015 – August 2017</p>
-        <p>Software Engineer</p>
-      </dt>
-      <dd>
-        Worked on a large code base with a team of interns and co-ops using pair
+      Puppet."
+    />
+    <ExperienceEntry
+      company="Vantiv"
+      location="Lowell, MA"
+      dateRange="May 2015 – August 2017"
+      jobTitle="Senior Software Engineer, Software Engineer"
+      description="Worked on a large code base with a team of interns and co-ops using pair
         programming. Projects included a C# XML parsing SDK, working with Spring
-        Framework, working with Groovy/Grails, and working with Gradle.
-      </dd>
-      <dt>
-        Litle & Co, Lowell, MA, May 2013 – August 2013, May 2014 – August 2014
-        <p>Software Engineer Intern</p>
-      </dt>
-      <dd>
-        Worked on a large code base with a team of interns and co-ops using pair
+        Framework, working with Groovy/Grails, and working with Gradle."
+    />
+    <ExperienceEntry
+      company="Litle & Co"
+      location="Lowell, MA"
+      dateRange="May 2013 – August 2013, May 2014 – August 2014"
+      jobTitle="Software Engineer Intern"
+      description="Worked on a large code base with a team of interns and co-ops using pair
         programming. Projects included a C# XML parsing SDK, working with Spring
-        Framework, working with Groovy/Grails, and working with Gradle.
-      </dd>
-      <dt>
-        WPI Helpdesk, Worcester, MA , September 2012 – May 2015
-        <p>Helpdesk Specialist</p>
-      </dt>
-      <dd>
-        Provided remote troubleshooting and support through phone, e-mail and
-        in-person for WPI faculty, staff, and students.
-      </dd>
-    </dl>
+        Framework, working with Groovy/Grails, and working with Gradle."
+    />
+    <ExperienceEntry
+      company="WPI Helpdesk"
+      location="Worcester, MA"
+      dateRange="September 2012 – May 2015"
+      jobTitle="Helpdesk Specialist"
+      description="Provided remote troubleshooting and support through phone, e-mail and
+        in-person for WPI faculty, staff, and students."
+    />
   </div>
 )
 
--- src/components/resume/experience/experienceEntry.tsx
+++ src/components/resume/experience/experienceEntry.tsx
@@ -4,31 +4,47 @@ import Row from "react-bootstrap/Row"
 
 interface ExperienceEntryProperties {
   company: string
+  location: string
   dateRange: string
   jobTitle: string
   description: string
 }
 
 const ExperienceEntry = (properties: ExperienceEntryProperties) => (
-  <Row>
-    <Row>
-      <Row>
+  <Row noGutters={true}>
+    <Col>
+      <Row noGutters={true}>
         <Col>
-          <p className="text-left">{properties.company}</p>
-        </Col>
-        <Col>
-          <p className="text-right">{properties.dateRange}</p>
+          <Row className="justify-content-between" noGutters={true}>
+            <Col xs="auto">
+              <p className="font-weight-bold mb-0 text-left">
+                {properties.company}
+              </p>
+              <p className="font-weight-bold mb-0 text-left">
+                {properties.location}
+              </p>
+            </Col>
+            <Col xs="auto">
+              <p className="font-weight-bold mb-0 text-right">
+                {properties.dateRange}
+              </p>
+            </Col>
+          </Row>
+          <Row noGutters={true}>
+            <Col>
+              <p className="font-weight-bold mb-0 text-left">
+                {properties.jobTitle}
+              </p>
+            </Col>
+          </Row>
         </Col>
       </Row>
-      <Row>
-        <p className="text-left">{properties.jobTitle}</p>
+      <Row noGutters={true}>
+        <Col>
+          <p>{properties.description}</p>
+        </Col>
       </Row>
-    </Row>
-    <Row>
-      <Col>
-        <p>{properties.description}</p>
-      </Col>
-    </Row>
+    </Col>
   </Row>
 )
 
