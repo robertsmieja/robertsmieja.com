f11c66730dd6de0165c17eec3341fed8d3679dc2
Robert Smieja <robertsmieja@users.noreply.github.com>
2019-06-04 00:13:59 -0400

    Cleanup imports

--- src/pages/presentations.tsx
+++ src/pages/presentations.tsx
@@ -1,8 +1,5 @@
 import React from "react"
 
-import { faGithub } from "@fortawesome/free-brands-svg-icons"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { Link } from "gatsby"
 import Container from "react-bootstrap/Container"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
