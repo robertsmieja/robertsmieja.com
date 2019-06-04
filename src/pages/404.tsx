18f3ced025526fb0db02a663179487768b26b19d
Robert Smieja <robertsmieja@users.noreply.github.com>
2019-06-04 19:21:17 -0400

    Add code to prevent 404 page flashing on load

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -4,14 +4,17 @@ import Container from "react-bootstrap/Container"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <Container>
-      <h1>404, Not Found</h1>
-      <p>You just hit a route that doesn't exist...</p>
-    </Container>
-  </Layout>
-)
+const browser = typeof window !== "undefined" && window
+
+const NotFoundPage = () =>
+  browser && (
+    <Layout>
+      <SEO title="404: Not found" />
+      <Container>
+        <h1>404, Not Found</h1>
+        <p>You just hit a route that doesn't exist...</p>
+      </Container>
+    </Layout>
+  )
 
 export default NotFoundPage
