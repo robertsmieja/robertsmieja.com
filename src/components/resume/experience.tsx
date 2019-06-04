fecb51b243b99466bb28d14509ea02d1d3f64fea
Robert Smieja <robertsmieja@users.noreply.github.com>
2019-06-03 22:51:40 -0400

    Update experience entries

--- src/components/resume/experience.tsx
+++ src/components/resume/experience.tsx
@@ -14,27 +14,28 @@ const Experience = () => (
       dateRange="August 2017 – Present"
       jobTitle="Senior Software Engineer"
       description="Worked on the WorldPay integration with AmazonPay as the first acquirer
-        to partner with AmazonPay. Currently working as a Automation/DevOps
-        working on development and production infrastructure automation in
-      Puppet."
+      to partner with AmazonPay, and on Vantiv eCommerce iQ Chargeback Engine as part of Visa Claims Resolution enchancements.
+      Currently working as a Automation/DevOps working on development and production infrastructure automation in Puppet."
     />
     <ExperienceEntry
       company="Vantiv"
       location="Lowell, MA"
       dateRange="May 2015 – August 2017"
       jobTitle="Senior Software Engineer, Software Engineer"
-      description="Worked on a large code base with a team of interns and co-ops using pair
-        programming. Projects included a C# XML parsing SDK, working with Spring
-        Framework, working with Groovy/Grails, and working with Gradle."
+      description="Worked on a large Java code base as part of System and Platform teams.
+      Projects include:
+      Ant to Gradle conversion
+      Grails 1.X to 2.X Upgrade
+      Coherence Upgrades and Improvements"
     />
     <ExperienceEntry
       company="Litle & Co"
       location="Lowell, MA"
       dateRange="May 2013 – August 2013, May 2014 – August 2014"
       jobTitle="Software Engineer Intern"
-      description="Worked on a large code base with a team of interns and co-ops using pair
+      description="Worked on a large Java code base with a team of interns and co-ops using pair
         programming. Projects included a C# XML parsing SDK, working with Spring
-        Framework, working with Groovy/Grails, and working with Gradle."
+        Framework, and working with Groovy/Grails."
     />
     <ExperienceEntry
       company="WPI Helpdesk"
