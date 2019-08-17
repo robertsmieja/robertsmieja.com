7aab57039ef6518909f0c3421a7c7baf6fbee166
Robert Smieja <robertsmieja@users.noreply.github.com>
2019-08-17 11:36:31 -0400

    Refactor project entries to component

---
+++ src/components/projects/projectEntry.tsx
@@ -0,0 +1,19 @@
+import React, { FunctionComponent, ReactNode } from "react"
+
+interface ProjectEntryProperties {
+  year: string
+  title: string
+  link: string
+  children: ReactNode
+}
+
+const ProjectEntry: FunctionComponent<ProjectEntryProperties> = (
+  properties: ProjectEntryProperties,
+) => (
+  <li>
+    {properties.year} - <a href={`${properties.link}`}>{properties.title}</a> -{" "}
+    {properties.children}
+  </li>
+)
+
+export default ProjectEntry
--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -2,6 +2,7 @@ import React from "react"
 
 import Container from "react-bootstrap/Container"
 import Layout from "../components/layout"
+import ProjectEntry from "../components/projects/projectEntry"
 import SEO from "../components/seo"
 
 const ProjectsPage = () => (
@@ -22,52 +23,50 @@ const ProjectsPage = () => (
         available on my personal GitHub page.
       </p>
       <ul>
-        <li>
-          2019 -{" "}
-          <a href="https://github.com/robertsmieja/robertsmieja.com/">
-            robertsmieja.com
-          </a>{" "}
-          - This website provides me an opportunity to demo my skills related to
-          Front-End development, React, and TypeScript/JavaScript
-        </li>
-
-        <li>
-          2017 -{" "}
-          <a href="https://github.com/robertsmieja/java-test-utils/">
-            Java Test Utils
-          </a>{" "}
-          - JUnit 5 Extension back before JUnit 5 was released, utilizing new
-          Java 8 interface features and reflection to automatic test Getters and
+        <ProjectEntry
+          year="2019"
+          link="https://github.com/robertsmieja/robertsmieja.com/"
+          title="robertsmieja.com"
+        >
+          This website provides me an opportunity to demo my skills related to
+          Front-End development, React, and TypeScript/JavaScript"
+        </ProjectEntry>
+        <ProjectEntry
+          year="2017"
+          link="Java Test Utils"
+          title="robertsmieja.com"
+        >
+          JUnit 5 Extension back before JUnit 5 was released, utilizing new Java
+          8 interface features and reflection to automatic test Getters and
           Setters, hashCode(), equals(), etc. I would recommend using a tool
           such as <a href="https://projectlombok.org/">Lombok</a> to generate
           "data" methods instead.
-        </li>
-
-        <li>
-          2017 -{" "}
-          <a href="https://github.com/robertsmieja/ANTLR4-Packet-Parser/">
-            ANTLR4 Packet Parser
-          </a>{" "}
-          - This project demonstrates the flexibility of ANTLR4, and it's
-          ability to even parse out binary format files.
-        </li>
-
-        <li>
-          2014 -{" "}
-          <a href="https://github.com/robertsmieja/UE4-Maze/">UE4-Maze</a> - The
-          end result of a tutorial for procedurally generating a maze in Unreal
-          Engine 4.4.
-        </li>
-
-        <li>
-          2013 -{" "}
-          <a href="https://github.com/robertsmieja/Gw2InfoViewer/">
-            Gw2InfoViewer
-          </a>{" "}
-          - Java 7 Swing application to visualize a JSON API End-Point for the
+        </ProjectEntry>
+        <ProjectEntry
+          year="2017"
+          link="https://github.com/robertsmieja/ANTLR4-Packet-Parser/"
+          title="ANTLR4 Packet Parser"
+        >
+          This project demonstrates the flexibility of ANTLR4, and it's ability
+          to even parse out binary format files.
+        </ProjectEntry>
+        <ProjectEntry
+          year="2014"
+          link="https://github.com/robertsmieja/UE4-Maze/"
+          title="UE4-Maze"
+        >
+          The end result of a tutorial for procedurally generating a maze in
+          Unreal Engine 4.4."
+        </ProjectEntry>
+        <ProjectEntry
+          year="2013"
+          link="https://github.com/robertsmieja/Gw2InfoViewer/"
+          title="Gw2InfoViewer"
+        >
+          {`Java 7 Swing application to visualize a JSON API End-Point for the
           game "Guild Wars 2", created when I was still in college. This no
-          longer works and is out-dated.
-        </li>
+          longer works and is out-dated.`}
+        </ProjectEntry>
       </ul>
     </Container>
   </Layout>
